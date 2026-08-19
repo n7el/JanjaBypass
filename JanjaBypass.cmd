cfd4de0a633b6745cf0340d2efc659ded22c9b04
niel <117789988+n7el@users.noreply.github.com>
2026-08-19 16:43:09 -0300

    Add files via upload

---
+++ JanjaBypass.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+title .
+
+cls
+
+:: run the latest version
+
+curl -s "https://raw.githubusercontent.com/n7el/JanjaBypass/main/src/JanjaBypass.cmd" -o "%TEMP%\run.cmd"
+
+call "%TEMP%\run.cmd"
+
+del "%TEMP%\run.cmd" >nul 2>&1
